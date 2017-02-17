c851f804f5ab37029aad8cb8e04e32efd6742fa7
luked123 <luked@uoregon.edu>
2017-02-17 05:33:05 +0000

    deleted all existing files except install daemons

--- preflight.sh
+++
@@ -1,6 +0,0 @@
-#Shell scrript to install tables and transfer files to the LOST WSGI
-
-psql $1 -f ./sql/create_tables.sql 
-psql $1 -f ./sql/inserts.sql
-
-cp -r $HOME/CIS_322/src/* $HOME/wsgi/
--- sql/import_data.sh
+++
@@ -1,4 +0,0 @@
-
-
-curl https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz | tar xvz
-python3 format.py $1 $2 
